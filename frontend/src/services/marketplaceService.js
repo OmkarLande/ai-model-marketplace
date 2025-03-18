15ff8f25115f8c00787b39ef58bcdd32b6c4b034
OmkarLande <omkarrlande.534@gmail.com>
2025-03-19 01:13:30 +0530

    enhance error handling in fetchModels function and improve logging

--- frontend/src/services/marketplaceService.js
+++ frontend/src/services/marketplaceService.js
@@ -1,13 +1,14 @@
 export const fetchModels = async () => {
     try {
-        const response = await fetch("http://localhost:5000/api/models/ai-models"); // Adjust URL as per your backend
-        console.log("response", response)
+        const response = await fetch("http://localhost:5000/api/models/ai-models");
+        console.log("Response status:", response);
         if (!response.ok) {
             throw new Error("Failed to fetch models.");
         }
         return await response.json();
     } catch (error) {
         console.error("Error fetching models:", error);
+        console.error("Error details:", error.message);
         return [];
     }
 };
