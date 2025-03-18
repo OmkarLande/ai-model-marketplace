76b16b83e02846728c16ece053fa9b7a7c87880f
OmkarLande <omkarrlande.534@gmail.com>
2025-03-18 22:32:28 +0530

    add login route and integrate cookie-parser middleware

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const { signup } = require("../controllers/UserController");
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
